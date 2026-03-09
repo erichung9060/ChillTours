a5dd7ab11393cf4218297cda381f0d1eb76a6e47
erichung9060 <rthung96@gmail.com>
2026-03-09 19:08:47 +0800

    chore(imports): Remove unused imports

--- components/planner/itinerary/components/edit-activity-dialog.tsx
+++ components/planner/itinerary/components/edit-activity-dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
 import {
   Dialog,
